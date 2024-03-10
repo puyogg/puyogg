d0a2976278b242c59b297cd3354d818d81ca9f2e
Phong Tran <s2lsoftener@gmail.com>
2024-03-09 19:35:08 -0500

    cron table

--- packages/puyoquest-db/src/models/index.ts
+++ packages/puyoquest-db/src/models/index.ts
@@ -1,2 +1,4 @@
 export * from './character.js';
 export * from './card.js';
+export * from './alias.js';
+export * from './cron-last-updated.js';
--- packages/puyoquest-db/src/models/init.ts
+++ packages/puyoquest-db/src/models/init.ts
@@ -2,11 +2,13 @@ import type { Sql } from 'postgres';
 import { CharacterModel } from './character.js';
 import { CardModel } from './card.js';
 import { AliasModel } from './alias.js';
+import { CronLastUpdatedModel } from './cron-last-updated.js';
 
 export interface Models {
   characterModel: CharacterModel;
   cardModel: CardModel;
   aliasModel: AliasModel;
+  cronLastUpdatedModel: CronLastUpdatedModel;
 }
 
 export const initModels = (sql: Sql): Models => {
@@ -14,5 +16,6 @@ export const initModels = (sql: Sql): Models => {
     characterModel: new CharacterModel(sql),
     cardModel: new CardModel(sql),
     aliasModel: new AliasModel(sql),
+    cronLastUpdatedModel: new CronLastUpdatedModel(sql),
   };
 };
